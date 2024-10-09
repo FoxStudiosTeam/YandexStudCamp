db1111e40e7daa9d843e7a1b4d43a14751a9c217
agniaendie <endienasg3@gmail.com>
2024-10-09 17:06:59 +0500

    initial commit

---
+++ camera-test/py.py
@@ -0,0 +1,30 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+rmsp_link = "rtsp://localhost:1935"
+
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+if cap.isOpened():
+    #cv2.CAP_FFMPEG,
+    cv2.VideoWriter(rmsp_link,0, fps, [width, height])
+
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        cv2.imshow('frame', gray)
+        if cv2.waitKey(1) == ord('q'):
+            break
