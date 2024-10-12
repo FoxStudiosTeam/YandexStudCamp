9a6c68615971ed8b77b3cd2e67b7b72fc2639e1d
AgniaEndie <endienasg2@gmail.com>
2024-10-13 04:31:28 +0500

    a* algo rewrite and program refactoring, render rw

--- python_src/fs_move_render.py
+++ python_src/fs_move_render.py
@@ -1,10 +1,12 @@
-from fs_move_simple import AStarPath, create_graph, Node
+from fs_move_simple import AStarPath, create_graph, Node, Direction
 from tkinter import *
 
 nodes = create_graph()
 a_star_path = AStarPath(nodes)
 
-queue = a_star_path.a_star_simple(nodes[0], nodes[196],nodes)
+# queue = a_star_path.a_star_simple(nodes[0], nodes[510],nodes)
+#path = a_star_path.a_star_simple(nodes[0], nodes[208])
+path = a_star_path.a_star_simple(nodes[0], nodes[526])
 
 root = Tk()
 
@@ -27,33 +29,69 @@ for node in nodes:
         maxY = node
     i += 0
 
-end_node = ((queue.queue)[len(queue.queue)-1])[1]
+# end_node = ((queue.queue)[len(queue.queue)-1])[1]
+end_node = path[len(path) - 1]
 for node in nodes:
-    x0 = node.x*10
-    x1 = x0+10
-    y0 = node.y*10
-    y1 = y0+10
+    x0 = node.x * 10
+    x1 = x0 + 10
+    y0 = node.y * 10
+    y1 = y0 + 10
     color = 'white'
-    if node == end_node: canvas.create_rectangle(x0, y0, x1, y1, fill='orange')
+    if node == end_node:
+        canvas.create_rectangle(x0, y0, x1, y1, fill='orange')
 
     elif node.is_block == True:
         canvas.create_rectangle(x0, y0, x1, y1, fill='red')
     else:
-        canvas.create_rectangle(x0,y0,x1,y1,fill=color)
-    
+        canvas.create_rectangle(x0, y0, x1, y1, fill=color)
 
-path = []
-while len(queue.queue) > 0:
-    item : tuple[int, Node] = queue.get()
-    path.append(item[1])
+# path = []
+# while len(queue.queue) > 0:
+#     item : tuple[int, Node] = queue.get()
+#     path.append(item[1])
 
-path.reverse()
+# path.reverse()
+
+
+# for i in range (len(path)-1):
+#     x0 = path[i].x*10 + 5
+#     y0 = path[i].y*10 + 5
+#     x1 = path[i+1].x*10 + 5
+#     y1 = path[i+1].y*10 + 5
+#     canvas.create_line(x0, y0, x1, y1, fill='blue', width=3)
+
+
+
+
 for i in range (len(path)-1):
-    x0 = path[i].x*10 + 5
-    y0 = path[i].y*10 + 5
+    elem = path[i]
+
+    x0 = elem.x * 10 + 5
+    y0 = elem.y*10 + 5
     x1 = path[i+1].x*10 + 5
     y1 = path[i+1].y*10 + 5
-    canvas.create_line(x0, y0, x1, y1, fill='blue', width=3)
 
-canvas.pack()
+    fill_color = 'black'
+    if elem.direction == Direction.FORWARD:
+        fill_color = 'blue'
+    elif elem.direction == Direction.BACK:
+        fill_color = 'red'
+    elif elem.direction == Direction.BACK_LEFT:
+        fill_color = 'orange'
+    elif elem.direction == Direction.BACK_RIGHT:
+        fill_color = 'green'
+    elif elem.direction == Direction.FORWARD_LEFT:
+        fill_color = 'purple'
+    elif elem.direction == Direction.FORWARD_RIGHT:
+        fill_color = 'cyan'
+    elif elem.direction == Direction.LEFT:
+        fill_color = 'pink'
+    elif elem.direction == Direction.RIGHT:
+        fill_color = 'yellow'
+
+    print(f'x :{elem.x} y: {elem.y} direction: {elem.direction}')
+
+    canvas.create_line(x0, y0, x1, y1, fill=fill_color, width=3)
+
+canvas.grid(row=0, column=0)
 root.mainloop()
