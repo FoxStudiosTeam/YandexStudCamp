8b90fe557d2c75d2f388b415e27f9180a874be1e
agniaendie <endienasg3@gmail.com>
2024-10-14 18:07:28 +0500

    fix graph

--- python_src/fs_movement.py
+++ python_src/fs_movement.py
@@ -4,13 +4,14 @@ from fs_move_simple import create_graph, AStarPath
 import fs_event as fs_ev
 from fs_motor import FSMover
 
-a_star_path = AStarPath()
-nodes = create_graph()
-path = a_star_path.a_star_simple(nodes[0], nodes[208], nodes)
+
 
 @fs_ev.bus.on('first_move')
 def first_step(fs_motor: FSMover):
-        for elem in path:
-            fs_ev.bus.emit("move", fs_motor, elem.direction)
-            time.sleep(1)
-        fs_ev.bus.emit("stop", fs_motor)
+    a_star_path = AStarPath()
+    nodes = create_graph()
+    path = a_star_path.a_star_simple(nodes[0], nodes[208], nodes)
+    for elem in path:
+        fs_ev.bus.emit("move", fs_motor, elem.direction)
+        time.sleep(1)
+    fs_ev.bus.emit("stop", fs_motor)
