d05f6fba50687629b0ff0873240dba1d722ae4f1
Danil1485 <danil.saykin.2016@gmail.com>
2024-10-14 10:23:25 +0500

    pre-final translate recent files

--- python_src/xr_voice.py
+++ python_src/xr_voice.py
@@ -1,18 +1,18 @@
-# # coding:utf-8
+# # coding: utf-8
 # """
-# 树莓派WiFi无线视频小车机器人驱动源码
-# 作者：Sence
-# 版权所有：小R科技（深圳市小二极客科技有限公司www.xiao-r.com）；WIFI机器人网论坛 www.wifi-robots.com
-# 本代码可以自由修改，但禁止用作商业盈利目的！
-# 本代码已申请软件著作权保护，如有侵权一经发现立即起诉！
+# Wi-Fi видеоробот-тележка на Raspberry Pi
+# Автор: Sence
+# Все права защищены: Xiao-R Technology (глубокая компания Shenzhen Xiaoer Geek Technology Co., Ltd.) ; WIFI робот форум www.wifi-robots.com
+# Этот код может быть свободно изменен, но запрещено использовать его в коммерческих целях!
+# Этот код уже подан на получение авторских прав на программное обеспечение, поэтому немедленно подавайте иск, если обнаружите нарушение!
 # """
 # """
 # @version: python3.7
-# @Author  : xiaor
-# @Explain :语音模块
-# @Time    :2020/05/09
-# @File    :xr_voice.py
-# @Software: PyCharm
+# @Автор : xiaor
+# @Объяснение : Модуль голоса
+# @Дата : 2020/05/09
+# @Файл : xr_voice.py
+# @Программное обеспечение: PyCharm
 # """
 import serial
 import time
@@ -20,56 +20,56 @@ import xr_config as cfg
 
 
 class Voice(object):
-	def __init__(self):
-		self.ser = serial.Serial("/dev/ttyS0", 9600)
-		pass
+    def __init__(self):
+        self.ser = serial.Serial("/dev/ttyS0", 9600)
+        pass
 
-	def run(self):
-		while True:
-			while self.ser.inWaiting() > 0:
-				time.sleep(0.05)
-				n = self.ser.inWaiting()
-				myout = self.ser.read(n)
-				self.get_voice(myout)
-				dat = int.from_bytes(myout, byteorder='big')
-				print('%#x' % dat)
+    def run(self):
+        while True:
+            while self.ser.inWaiting() > 0:
+                time.sleep(0.05)
+                n = self.ser.inWaiting()
+                myout = self.ser.read(n)
+                self.get_voice(myout)
+                dat = int.from_bytes(myout, byteorder='big')
+                print('%#x' % dat)
 
-			time.sleep(0.5)
+            time.sleep(0.5)
 
-	def get_voice(self, data):
-		cfg.VOICE_MOD = cfg.VOICE_MOD_SET['normal']
-		if len(data) < cfg.RECV_LEN:  # 不符合接收长度标准
-			# print('data len %d:'%len(data))
-			return cfg.VOICE_MOD
-		if data[0] == 0xff and data[len(data) - 1] == 0xff:  # 如果包头和包尾是0xff则符合小二科技通信协议
-			buf = []  # 定义一个列表
-			for i in range(1, 4):  # 获取协议包中间3位的数据
-				buf.append(data[i])  # 往buf中添加数据
-			if buf[0] == 0xf5 and buf[1] == 0x01:
-				if buf[2] == 0x01:
-					cfg.VOICE_MOD = cfg.VOICE_MOD_SET['normal']
-				elif buf[2] == 0x02:
-					cfg.VOICE_MOD = cfg.VOICE_MOD_SET['openlight']
-				elif buf[2] == 0x03:
-					cfg.VOICE_MOD = cfg.VOICE_MOD_SET['closelight']
-				elif buf[2] == 0x06:
-					cfg.VOICE_MOD = cfg.VOICE_MOD_SET['forward']
-				elif buf[2] == 0x07:
-					cfg.VOICE_MOD = cfg.VOICE_MOD_SET['back']
-				elif buf[2] == 0x08:
-					cfg.VOICE_MOD = cfg.VOICE_MOD_SET['left']
-				elif buf[2] == 0x09:
-					cfg.VOICE_MOD = cfg.VOICE_MOD_SET['right']
-				elif buf[2] == 0x0A:
-					cfg.VOICE_MOD = cfg.VOICE_MOD_SET['stop']
-				elif buf[2] == 0x0B:
-					cfg.VOICE_MOD = cfg.VOICE_MOD_SET['nodhead']
-				elif buf[2] == 0x0C:
-					cfg.VOICE_MOD = cfg.VOICE_MOD_SET['shakehead']
-		return cfg.VOICE_MOD
+    def get_voice(self, data):
+        cfg.VOICE_MOD = cfg.VOICE_MOD_SET['normal']
+        if len(data) < cfg.RECV_LEN:  # Если длина данных меньше ожидаемого значения
+            # print('data len %d:'%len(data))
+            return cfg.VOICE_MOD
+        if data[0] == 0xff and data[len(data) - 1] == 0xff:  # Если начало и конец пакета равны 0xff
+            buf = []  # Определить список
+            for i in range(1, 4):  # Получить данные из середины пакета
+                buf.append(data[i])  # Добавить данные в buf
+            if buf[0] == 0xf5 and buf[1] == 0x01:
+                if buf[2] == 0x01:
+                    cfg.VOICE_MOD = cfg.VOICE_MOD_SET['normal']
+                elif buf[2] == 0x02:
+                    cfg.VOICE_MOD = cfg.VOICE_MOD_SET['openlight']
+                elif buf[2] == 0x03:
+                    cfg.VOICE_MOD = cfg.VOICE_MOD_SET['closelight']
+                elif buf[2] == 0x06:
+                    cfg.VOICE_MOD = cfg.VOICE_MOD_SET['forward']
+                elif buf[2] == 0x07:
+                    cfg.VOICE_MOD = cfg.VOICE_MOD_SET['back']
+                elif buf[2] == 0x08:
+                    cfg.VOICE_MOD = cfg.VOICE_MOD_SET['left']
+                elif buf[2] == 0x09:
+                    cfg.VOICE_MOD = cfg.VOICE_MOD_SET['right']
+                elif buf[2] == 0x0A:
+                    cfg.VOICE_MOD = cfg.VOICE_MOD_SET['stop']
+                elif buf[2] == 0x0B:
+                    cfg.VOICE_MOD = cfg.VOICE_MOD_SET['nodhead']
+                elif buf[2] == 0x0C:
+                    cfg.VOICE_MOD = cfg.VOICE_MOD_SET['shakehead']
+            return cfg.VOICE_MOD
 
 if __name__ == "__main__":
-	ser = Voice()
-	while True:
-		print("run voice ctl")
-		ser.run()
+    ser = Voice()
+    while True:
+        print("run voice ctl")
+        ser.run()
