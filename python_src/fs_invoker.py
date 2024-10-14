2279013b966686a6fb30397acd3664e6c4f7937e
agniaendie <endienasg3@gmail.com>
2024-10-14 13:15:43 +0500

    add self parameter fs_motor

--- python_src/fs_invoker.py
+++ python_src/fs_invoker.py
@@ -9,4 +9,4 @@ from fs_motor import FSMover
 def test(fs_motor: FSMover):
     fs_ev.bus.emit('move', fs_motor,Direction.FORWARD)
     time.sleep(2)
-    fs_ev.bus.emit('stop')
+    fs_ev.bus.emit('stop',fs_motor)
