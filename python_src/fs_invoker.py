1bfb9206fc35a59ec4d25120b3dc26e77af14158
agniaendie <endienasg3@gmail.com>
2024-10-14 13:03:21 +0500

    try to fix test

--- python_src/fs_invoker.py
+++ python_src/fs_invoker.py
@@ -1,10 +1,12 @@
 import time
 
 import fs_event as fs_ev
+from xr_startmain import fs_motor
+
 from fs_move_simple import Direction
 
 
 def test():
-    fs_ev.bus.emit('move', Direction.FORWARD)
+    fs_ev.bus.emit('move', fs_motor,Direction.FORWARD)
     time.sleep(2)
     fs_ev.bus.emit('stop')
