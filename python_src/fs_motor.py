1d358f9460ab3dc8dfbf6a3f537c53bf861d682e
agniaendie <endienasg3@gmail.com>
2024-10-14 10:40:36 +0500

    add test movement with event bus

--- python_src/fs_motor.py
+++ python_src/fs_motor.py
@@ -2,14 +2,17 @@ import time
 
 import xr_config as cfg
 from fs_move_simple import Direction
+from python_src.xr_startmain import bus
 from xr_motor import RobotDirection
 
 class FSMover(RobotDirection):
 
+    @bus.on('stop')
     def stop(self):
         self.stop()
 
-    def move(self, direction: Direction, last_direction: Direction):
+    @bus.on('move')
+    def move(self, direction: Direction):
         if direction == Direction.FORWARD:
             self.forward()
         if direction == Direction.FORWARD_LEFT:
@@ -26,3 +29,4 @@ class FSMover(RobotDirection):
             self.stop()
         if direction == Direction.BACK_RIGHT:
             self.stop()
+
