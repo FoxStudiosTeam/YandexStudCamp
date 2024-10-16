33444d008a7b39296264cc1ab11b285fa4a61b8c
Kaiva-Morphin <kaivamorphin@gmail.com>
2024-10-16 08:04:00 +0300

    raycast

--- python_src/fs_raycast_display.py
+++ python_src/fs_raycast_display.py
@@ -33,65 +33,184 @@ def draw_grid():
 
 def unchecked_cell(x, y, c=GREEN):
     draw_rectangle(
-        math.ceil(x * CELL_SIZE[0] - CELL_SIZE[0] / 8), math.ceil(y * CELL_SIZE[1] - CELL_SIZE[1] / 8), math.ceil(CELL_SIZE[0]/4), math.ceil(CELL_SIZE[1]/4), c)
+        math.floor(x * CELL_SIZE[0] + CELL_SIZE[0] / 4), 
+        math.floor(y * CELL_SIZE[1] + CELL_SIZE[1] / 4), 
+        math.floor(CELL_SIZE[0]/2), 
+        math.floor(CELL_SIZE[1]/2), c)
 
+
+#def has_intersection(start, end, diag_check=False):
+#    x1 = start[0] + 0.5
+#    y1 = start[1] + 0.5
+#    x2 = end[0] + 0.5
+#    y2 = end[1] + 0.5
+#    if y1 == y2:
+#        for x in range(math.floor(min(x1, x2)), math.ceil(max(x1, x2))):
+#            unchecked_cell(x + 0.5, y1)
+#        return
+#    if x1 == x2:
+#        for y in range(math.floor(min(x1, x2)), math.ceil(max(x1, x2))):
+#            unchecked_cell(x1, y + 0.5)
+#        return
+#    if x1 == x2 or y1 == y2: return False
+#    if abs(x1 - x2) > abs(y1 - y2):
+#        prevx = x1 if (y1 - y2 < 0) else x2
+#        m = (y2 - y1) / (x2 - x1)
+#        y = min(y1, y2)
+#        #unchecked_cell(prevx, y, RED)
+#        while y <= max(y1, y2):
+#            # y = mx + b
+#            # x = (y - b) / m
+#            x = (y - y1 + 0.5) / m + x1
+#            #if not (min(x1, x2) < x < max(x1, x2)):
+#            #    break
+#            #unchecked_cell(x, y + 0.5)
+#            #if diag_check and abs(x) % 1 < 0.001:
+#            #    if (min(x1, x2) < x < max(x1, x2)):
+#            #        unchecked_cell(x, y + 0.5, RED)
+#            #        unchecked_cell(x - 0.5, y, RED)
+#            #        unchecked_cell(x + 0.5, y, RED)
+#            #        unchecked_cell(x - 0.5, y + 1, RED)
+#            #        unchecked_cell(x + 0.5, y + 1, RED)
+#            mix = math.floor(min(prevx, math.floor(x)))
+#            mx = math.floor(max(prevx, math.floor(x)))
+#            for nx in range(mix, mx + 1):
+#                if not (min(x1, x2) < nx < max(x1, x2)):
+#                    break
+#                unchecked_cell(nx + 0.5, y)
+#            prevx = math.floor(x)
+#            #unchecked_cell(mix + 0.5, y, RED)
+#            #
+#            #unchecked_cell(prevx, y - 0.5)
+#            y += 1
+        
+'''
+
+void line_DDA_subpixel(int x0,int y0,int x1,int y1,int col) // DDA subpixel -> thick
+    {
+    int kx,ky,c,i,xx,yy,dx,dy;
+    x1-=x0; kx=0; if (x1>0) kx=+1; if (x1<0) { kx=-1; x1=-x1; } x1++;
+    y1-=y0; ky=0; if (y1>0) ky=+1; if (y1<0) { ky=-1; y1=-y1; } y1++;
+    if (x1>=y1)
+     for (c=x1,i=0;i<x1;i++,x0+=kx)
+        {
+        pnt(x0,y0,col); // this is normal pixel the two below are subpixels 
+        c-=y1; if (c<=0) { if (i!=x1-1) pnt(x0+kx,y0,col); c+=x1; y0+=ky; if (i!=x1-1) pnt(x0,y0,col); }
+        }
+    else
+     for (c=y1,i=0;i<y1;i++,y0+=ky)
+        {
+        pnt(x0,y0,col); // this is normal pixel the two below are subpixels 
+        c-=x1; if (c<=0) { if (i!=y1-1) pnt(x0,y0+ky,col); c+=y1; x0+=kx; if (i!=y1-1) pnt(x0,y0,col); }
+        }
+    }
+
+'''
 
 def has_intersection(start, end, diag_check=False):
-    x1 = start[0] + 0.5
-    y1 = start[1] + 0.5
-    x2 = end[0] + 0.5
-    y2 = end[1] + 0.5
-    if y1 == y2:
-        for x in range(math.floor(min(x1, x2)), math.ceil(max(x1, x2))):
-            unchecked_cell(x + 0.5, y1)
-        return
-    if x1 == x2:
-        for y in range(math.floor(min(x1, x2)), math.ceil(max(x1, x2))):
-            unchecked_cell(x1, y + 0.5)
-        return
-    if x1 == x2 or y1 == y2: return False
-
-
-    if abs(x1 - x2) > abs(y1 - y2):
-        positivex = True if x1 < x2 else False
-        prevx = math.floor(min(x1, x2)) # if (x1 - x2 <= 0) == (y1 - y2 <= 0) else max(x1, x2)
-        m = (y2 - y1) / (x2 - x1)
-        y = min(y1, y2)
-        
-        unchecked_cell(prevx, y, RED)
-
-        while y <= max(y1, y2):
-            # y = mx + b
-            # x = (y - b) / m
-            x = (y - y1 + 0.5) / m + x1
-            #if not (min(x1, x2) < x < max(x1, x2)):
-            #    break
-            unchecked_cell(x, y + 0.5)
-            #if diag_check and abs(x) % 1 < 0.001:
-            #    if (min(x1, x2) < x < max(x1, x2)):
-            #        unchecked_cell(x, y + 0.5, RED)
-            #        unchecked_cell(x - 0.5, y, RED)
-            #        unchecked_cell(x + 0.5, y, RED)
-            #        unchecked_cell(x - 0.5, y + 1, RED)
-            #        unchecked_cell(x + 0.5, y + 1, RED)
-            #mix = min(prevx, math.floor(x))
-            #mx = max(prevx, math.floor(x))
-            #for nx in range(mix, mx + 1):
-            #    unchecked_cell(nx + 0.5, y)
-            #    if not (min(x1, x2) < x < max(x1, x2)):
-            #        break
-            #unchecked_cell(mix + 0.5, y, RED)
-            #
-            #unchecked_cell(prevx, y - 0.5)
-            #prevx = math.floor(x)
-            y += 1
-        
+    x0 = start[0]
+    y0 = start[1]
+    x1 = end[0]
+    y1 = end[1]
+    x1 -= x0
+    y1 -= y0
+    kx = 0
+    ky = 0
+    c = 0
+    i = 0
+    if x1 > 0: kx = 1
+    if x1 < 0:
+        kx = -1
+        x1 = -x1
+    x1 += 1
+    if y1 > 0: ky = 1
+    if y1 < 0:
+        ky = -1
+        y1 = -y1
+    y1 += 1
+    if x1 >= y1:
+        c = x1
+        i = 0
+        while i < x1:
+            unchecked_cell(x0, y0)
+            c -= y1
+            if c <= 0:
+                if i != (x1 - 1): unchecked_cell(x0 + kx, y0)
+                c += x1
+                y0 += ky
+                if i != (x1 - 1): unchecked_cell(x0, y0)
+            x0 += kx
+            i += 1
+    else:
+        c = y1
+        i = 0
+        while i < y1:
+            unchecked_cell(x0, y0)
+            c -= x1
+            if c <= 0:
+                if i != (y1 - 1): unchecked_cell(x0, y0 + ky)
+                c += y1
+                x0 += kx
+                if i != (y1 - 1): unchecked_cell(x0, y0)
+            y0 += ky
+            i += 1
 
 
 
+def has_intersection2(start, end, diag_check=False):
+    x0 = start[0] + 0.5
+    y0 = start[1] + 0.5
+    x1 = end[0] + 0.5
+    y1 = end[1] + 0.5
+    kx = 0
+    ky = 0
+    c = 0
+    i = 0
+    x1 -= x0
+    y1 -= y0
+    if x1 > 0: kx += 1
+    if x1 < 0:
+        kx -= 1
+        x1 = -x1
+    x1 += 1
+    if y1 > 0: ky += 1
+    if y1 < 0:
+        ky -= 1
+        y1 = -y1
+    y1 += 1
+    if x1 >= y1:
+        c = x1
+        i = 0
+        while i < x1:
+            unchecked_cell(x0 + 0.5, y0)
+            c -= y1
+            if c <= 0:
+                if i != x1 - 1:
+                    unchecked_cell(x0 + kx + 0.5, y0)
+                c += x1
+                y0 += ky
+                if i != x1 - 1:
+                    unchecked_cell(x0 + 0.5, y0)
+            x0 += kx
+            i += 1
+    else:
+        c = y1
+        i = 0
+        while i < y1:
+            unchecked_cell(x0, y0 + 0.5)
+            c -= x1
+            if c <= 0:
+                if i != y1 - 1:
+                    unchecked_cell(x0, y0 + ky + 0.5)
+                c += y1
+                x0 += kx
+                if i != y1 - 1:
+                    unchecked_cell(x0, y0 + 0.5)
+            y0 += ky
+            i += 1
 
-
-
+    
+    
 
 START_POS = (0, 0)
 
