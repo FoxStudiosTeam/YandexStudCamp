5acc88591d9ac311124ae3fadcb310bde23b4ccb
aTekman <pavelefremov75026@gmail.com>
2024-10-17 14:10:57 +0500

    update

--- aTekman/robot_object_detection/src/data_preprocessing.py
+++ aTekman/robot_object_detection/src/data_preprocessing.py
@@ -1,7 +1,7 @@
 import os
 import cv2
 import numpy as np
-from src.config import IMAGE_WIDTH, IMAGE_HEIGHT
+from config import IMAGE_WIDTH, IMAGE_HEIGHT
 
 def load_images_from_folder(folder):
     images = []
@@ -13,4 +13,6 @@ def load_images_from_folder(folder):
     return np.array(images)
 
 def preprocess_labels(labels):
-    #код для предобработки меток return labels
+    label_map = {label: idx for idx, label in enumerate(set(labels))}
+    numeric_labels = np.array([label_map[label] for label in labels])
+    return numeric_labels
