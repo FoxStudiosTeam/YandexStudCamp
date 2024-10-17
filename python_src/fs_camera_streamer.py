ec4c82e5485d39db7c7b94a1802dcd5284c0302c
agniaendie <endienasg3@gmail.com>
2024-10-17 16:55:25 +0500

    updated camera stream and onload service

---
+++ python_src/fs_camera_streamer.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask(__name__)
+
+def generate_frames():
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
