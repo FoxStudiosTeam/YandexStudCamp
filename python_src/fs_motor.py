69e8eb5f6244e9bc5bc4ee2eb097af28721f7659
Ar4ikoffc <artem.dln@mail.ru>
2024-10-18 16:30:24 +0500

    dodelano

--- python_src/fs_motor.py
+++ python_src/fs_motor.py
@@ -6,30 +6,34 @@ from xr_motor import RobotDirection
 
 class FSMover(RobotDirection):
 
+    cur_direction : Direction = None
+
     @fs_ev.bus.on('stop')
     def stop(self):
         super().stop()
 
     @fs_ev.bus.on('move')
-    def move(self, cur_direction : Direction, next_direction : Direction):
-        diff = next_direction.value - cur_direction.value
+    def move(self, next_direction : Direction):
+        if self.cur_direction == None:
+            self.cur_direction = Direction.BACK
+        diff = next_direction.value - self.cur_direction.value
         if diff != 0:
             super().stop()
             if diff < 0:
                 super().left()
                 time.sleep(abs(diff//45))
                 super().stop()
-                cur_direction = next_direction
+                self.cur_direction = next_direction
             elif diff > 0:
                 super().right()
                 time.sleep(diff//45)
                 super().stop()
-                cur_direction = next_direction
-        if cur_direction == next_direction:
+                self.cur_direction = next_direction
+        if self.cur_direction == next_direction:
             super().forward()
-            if cur_direction in [45, 135, 225, 315]:
+            if self.cur_direction in [45, 135, 225, 315]:
                 time.sleep(1.76)
-            elif cur_direction in [0, 90, 180, 270]: time.sleep(1.25)
+            elif self.cur_direction in [0, 90, 180, 270]: time.sleep(1.25)
     @fs_ev.bus.on('go-back')
     def go_back(self, sec):
         super().back()
