cb8464ad9772146fbe2ac6894e8ed1b8bda6c0a7
weednw <pavelefremov75026@gmail.com>
2024-10-21 22:38:56 +0500

    update neuro

---
+++ aTekman/robot_object_detection/cv2Yolodetected.py
@@ -0,0 +1,32 @@
+import cv2
+from ultralytics import YOLO
+
+model = YOLO('C:\\Users\\weednw\\PycharmProjects\\YandexStudCamp\\aTekman\\robot_object_detection\\src\\runs\\detect\\train16\\weights\\best.pt')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+
+    ret, frame = cap.read()
+    if not ret:
+        print("кадр не получен")
+        break
+
+    results = model(frame)
+    for result in results:
+        for box in result.boxes:
+
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+            confidence = box.conf[0]
+            class_id = int(box.cls[0])
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0, 70), 2)
+            label = f"ID: {class_id}, Conf: {confidence:.2f}"
+            cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    cv2.imshow(frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
