e1e44857bff107cbe97e01059e7e30275797dfed
weednw <pavelefremov75026@gmail.com>
2024-10-21 22:56:22 +0500

    update neuro

--- aTekman/robot_object_detection/cv2Yolodetected.py
+++ aTekman/robot_object_detection/cv2Yolodetected.py
@@ -3,6 +3,7 @@ from ultralytics import YOLO
 
 model = YOLO('C:\\Users\\weednw\\PycharmProjects\\YandexStudCamp\\aTekman\\robot_object_detection\\src\\runs\\detect\\train16\\weights\\best.pt')
 
+class_names = model.names
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -20,8 +21,13 @@ while True:
             confidence = box.conf[0]
             class_id = int(box.cls[0])
 
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0, 70), 2)
-            label = f"ID: {class_id}, Conf: {confidence:.2f}"
+            if class_id < len(class_names):
+                class_name = class_names[class_id]
+            else:
+                class_name = "Unknown"
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            label = f"{class_name} (ID: {class_id}), Conf: {confidence:.2f}"
             cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
     cv2.imshow(frame)
